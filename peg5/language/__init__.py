4c67991b314f125b9d581ce54fa246b72fb7b1ea
Pablo Jorge Eduardo Rodriguez <pr@tekii.com.ar>
2021-10-14 15:09:36 -0300

    some linting

--- peg5/language/__init__.py
+++ peg5/language/__init__.py
@@ -1,10 +1,11 @@
 from langkit.compile_context import CompileCtx
 
+
 def prepare_peg5_context() -> CompileCtx:
     from .lexer import p5_lexer
     from .parser import p5_grammar
     ctx = CompileCtx(lang_name='Peg5',
-                            short_name='Peg5',
-                            lexer=p5_lexer,
-                            grammar=p5_grammar)
+                     short_name='Peg5',
+                     lexer=p5_lexer,
+                     grammar=p5_grammar)
     return ctx
