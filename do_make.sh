9abe578868ef6f4710b6ab6d9172a7774f6f5702
Pablo Jorge Eduardo Rodriguez <pr@tekii.com.ar>
2021-10-26 17:26:26 -0300

    wip - moving  Manage class up

--- do_make.sh
+++ do_make.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-python3 peg5/manage.py make --library-types relocatable --debug --build-mode dev --verbosity debug --pretty-print $1 $2 $3 $4
+python3 manage.py make --library-types relocatable --debug --build-mode dev --verbosity debug --pretty-print $1 $2 $3 $4
 
 #                            --pass-on "emit railroad diagrams"
 #                            --library-types=static,static-pic,relocatable \
